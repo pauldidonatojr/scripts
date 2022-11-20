3174bbb4d9e63ae7903e750a118ea92943cda3df
Paul DiDonato <pjdidonato@galico.domainstacks.net>
2022-11-20 16:17:38 -0500

    push me

---
+++ algos/counting_sort.js
@@ -0,0 +1,89 @@
+/*
+consider data in range of 0 to 9
+1,4,1,2,7,5,2
+Index:
+| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
+| 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+
+- count each element in the given array and place the count
+at the appropriate index
+- store the count of each unique element in the count array
+- if any element repeats itself simply increase its count
+
+
+Counting Operations:
+
+**The Problem With Time**
+- Different machines will record different times
+- The same machine will record different times
+- For fast algorithms speed measurements may not be precise enouggh
+
+
+
+Counting Operations:
+
+function addUpTo (n) {
+    return n * (n + 1) / 2;
+}
+
+1 multiplication
+1 addition
+1 division
+
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+*/
+
+/*
+
+loop through the array and count
+
+
+*/
+
+const arr = [
+  {
+    user: {
+      count: 0,
+    },
+  },
+  {
+    user: {
+      count: 0,
+    },
+  },
+  {
+    user: {
+      count: 0,
+    },
+  },
+]
+
+// console.log(arr[0].user1.count + 5)
+
+const count = (x) => {
+  // let total = 0;
+
+  for (const element of x) {
+    for (const property in element) {
+      if (element[property].count === 0) {
+          element[property].count += 1
+          
+      }
+      //   console.log(
+      //     `ID: ${element[property].id}\nCount: ${element[property].count}`
+      //   )
+      if (element[property].id === 2) {
+        element[property].count += 1
+      }
+    }
+  }
+}
+
+count(arr)
+console.log(arr)
